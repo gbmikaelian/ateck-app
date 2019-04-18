1980fbf8c58f6383fcb691ccd63d305d3bc1c909
Garik <g.b.mikaelian@gmail.com>
2019-04-18 16:59:43 +0400

    element ui , fontawesome

--- src/main.js
+++ src/main.js
@@ -2,6 +2,29 @@ import Vue from 'vue';
 import App from '@/App.vue';
 import router from '@/router';
 import store from '@/store';
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import locale from 'element-ui/lib/locale/lang/en';
+
+import fontawesome from '@fortawesome/fontawesome';
+import brands from '@fortawesome/fontawesome-free-brands';
+import solid from '@fortawesome/fontawesome-free-solid';
+import regular from '@fortawesome/fontawesome-free-regular';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+fontawesome.library.add(brands, solid, regular);
+library.add(fas, faTwitter);
+Vue.component('icon', FontAwesomeIcon);
+dom.watch();
+
+Vue.use(ElementUI, {
+    locale
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
