674535eb233e323c299443a9a7ddc6d103c21d2f
Garik <g.b.mikaelian@gmail.com>
2019-04-18 16:49:00 +0400

    vuex

---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+
+Vue.use(Vuex);
+
+const state = {
+
+};
+
+const actions = {
+
+};
+
+const mutations = {
+
+};
+
+export default new Vuex.Store({
+    state,
+    actions,
+    mutations
+});
